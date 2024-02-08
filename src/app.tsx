5f2faab024a302e7e2a5b10542ce104a43b2921c
Robson <robsonhb@uninove.br>
2024-02-08 12:51:57 -0300

    Corrigindo os erros de deletar a nota e inserir via microfone do dipositivo

--- src/app.tsx
+++ src/app.tsx
@@ -78,12 +78,12 @@ export function App() {
       
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 auto-rows-[250px]">
 
-        <NewNoteCard onNoteCreated={onNoteCreated} onNoteDeleted={onNoteDeleted}/>
+        <NewNoteCard onNoteCreated={onNoteCreated} />
 
   
 
         {filteredNotes.map(note => {
-          return <NoteCard key={note.id} note={note}/>
+          return <NoteCard key={note.id} note={note} onNoteDeleted={onNoteDeleted}/>
         })}
 
       </div>
